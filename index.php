1b9ac254a8ede5d5132fc81b8740fc40d1074643
codeuronline <j.kasperski@codeur.online>
2022-04-06 12:17:32 +0200

    update->change name file

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 if (empty($_GET['slug'])) {
-    require "view/accueil.view.php";
+    require "views/accueil.view.php";
 } else {
     switch ($_GET['slug']) {
         case 'accueil':
-            require "view/accueil.view.php";
+            require "views/accueil.view.php";
             break;
             case 'videos':
-                require "view/videos.view.php";
+                require "views/video.view.php";
                 break;
     default:
             echo "<center><h1>Erreur 404 PAGE not Found</h1></center>";
