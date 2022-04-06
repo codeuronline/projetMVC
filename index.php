f33772a2ae0cf6c39fd6fb3f7762b14aa649e79e
codeuronline <j.kasperski@codeur.online>
2022-04-06 11:34:09 +0200

    update 404 page

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-if (empty($_GET['page'])) {
+if (empty($_GET['slug'])) {
     require "views/accueil.views.php";
 } else {
-    switch ($_GET['page']) {
+    switch ($_GET['slug']) {
         case 'accueil':
             require "views/accueil.views.php";
             break;
+    default:echo "<center><h1>Erreur 404 PAGE not Found</h1></center>";
     }
 }
