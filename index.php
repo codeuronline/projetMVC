63ef5012b64eb5e7a6591f7cb2fe97d5bb458e1c
codeuronline <j.kasperski@codeur.online>
2022-04-06 12:04:52 +0200

    update conttruction videos

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 if (empty($_GET['slug'])) {
-    require "views/accueil.views.php";
+    require "view/accueil.view.php";
 } else {
     switch ($_GET['slug']) {
         case 'accueil':
-            require "views/accueil.views.php";
+            require "view/accueil.view.php";
             break;
-    default:echo "<center><h1>Erreur 404 PAGE not Found</h1></center>";
+            case 'videos':
+                require "view/videos.view.php";
+                break;
+    default:
+            echo "<center><h1>Erreur 404 PAGE not Found</h1></center>";
     }
 }
---
+++ view/template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    <title><?=$titre?></title>
+</head>
+
+<body>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="accueil">Accueil</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="videos">Video</a>
+            </li>
+        </ul>
+    </div>
+    <?=$content?>
+</body>
+
+</html>
