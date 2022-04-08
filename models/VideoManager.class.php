5f0b8ab67f08da3a9663d39dc4b506721604c6ab
codeuronline <j.kasperski@codeur.online>
2022-04-08 16:45:59 +0200

    UPDATE->OK

--- models/VideoManager.class.php
+++ models/VideoManager.class.php
@@ -6,26 +6,19 @@ class VideoManager extends Model{
 
     public $videos;
     
-    public function ajoutVideo($video)
-    {
+    public function ajoutVideo($video){
         $this->videos[] = $video;
-}
-
-
-
-public function getVideos()
-{
-return $this->videos;
-
-
-}
+    }
+    
+    public function getVideos(){
+        return $this->videos;
+    }
 
     public function chargementVideos(){
         $req = $this->getBdd()->prepare("SELECT * FROM videos");
         $req->execute();
         $mesVideos = $req->fetchAll();
         $req->closeCursor();
-
         foreach($mesVideos as $video){
                     $this->ajoutVideo(new Video($video));
         }
@@ -33,7 +26,6 @@ return $this->videos;
     
     public function getVideoById($id){
         for ($i=0; $i < count($this->videos); $i++) {
-            var_dump($id."?=".$i);
             if ($this->videos[$i]->getId()=== $id){
                 return $this->videos[$i];
             }
@@ -41,22 +33,22 @@ return $this->videos;
     }
                     
     public function ajoutVideoBd($data){
-    extract($data);
-    $sql ="INSERT INTO videos(titre,duree,photo) VALUES (:titre,:duree,:photo) ";
-    $pdo= $this->getBdd()->prepare($sql);
-    foreach ($data as $key => $value) {
-    $pdo->bindValue(":".$key, $value,PDO::PARAM_STR); # code...
-    }
-    
-    $resultat=$pdo->execute();
-    $pdo->closeCursor;
-    if ($resultat>0) {
-    $data['id']=$this->getBdd()->lastInsertId();
-            $video = new Video($data) ;
-            $this->ajoutVideo($video);
-    }
+        extract($data);
+        $sql ="INSERT INTO videos(titre,duree,photo) VALUES (:titre,:duree,:photo) ";
+        $pdo= $this->getBdd()->prepare($sql);
+        foreach ($data as $key => $value) {
+        $pdo->bindValue(":".$key, $value,PDO::PARAM_STR); # code...
+        }
+        
+        $resultat=$pdo->execute();
+        $pdo->closeCursor;
+        if ($resultat>0) {
+        $data['id']=$this->getBdd()->lastInsertId();
+                $video = new Video($data) ;
+                $this->ajoutVideo($video);
+        }
     }
 
-    }
+}
 
     ?>
